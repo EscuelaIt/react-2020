abaea3050bfea1effc3643c7f1be494f3195ea27
Jonathan MirCha <jonmircha@gmail.com>
2020-03-30 12:50:33 -0600

    Updates

---
+++ precache-manifest.0e7f57e29f86a464ba7e11e193456f80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751e3f79af485c855ac209af3854fccf",
+    "url": "/EscuelaIt/react-2020/index.html"
+  },
+  {
+    "revision": "b9afb03d0228c4979425",
+    "url": "/EscuelaIt/react-2020/static/css/main.6dbe2ff7.chunk.css"
+  },
+  {
+    "revision": "6aa490eea494877af411",
+    "url": "/EscuelaIt/react-2020/static/js/2.49ccdd19.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/EscuelaIt/react-2020/static/js/2.49ccdd19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9afb03d0228c4979425",
+    "url": "/EscuelaIt/react-2020/static/js/main.91aa29fb.chunk.js"
+  },
+  {
+    "revision": "bf98750e8c5e96a1d40a",
+    "url": "/EscuelaIt/react-2020/static/js/runtime-main.4f189238.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EscuelaIt/react-2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
